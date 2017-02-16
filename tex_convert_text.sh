135c7c6556aa8917bb13fabc12a37c47a7d59db0
Hisham <hisham@gobolinux.org>
2017-02-15 22:59:29 -0200

    Update merging of subprojects

--- tex_convert_text.sh
+++ tex_convert_text.sh
@@ -3,5 +3,5 @@
 gawk '/BEGIN LYX TEXT/ { on=1 } { if (on) print } /END LYX TEXT/ { on=0 }' "$1" |\
 sed 's,\\subsection{,\\subsubsection{,g' |\
 sed 's,\\section{,\\subsection{,g;
-     s,\\~,\\textasciitilde{},g;'
-
+     s,\\~,\\textasciitilde{},g;
+     s,—,\\textemdash{},g;'
