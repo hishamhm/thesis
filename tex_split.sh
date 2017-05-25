8a5fd507fbacdfb26b018c2f3dcb6578cda5a356
Hisham <hisham@gobolinux.org>
2017-05-25 17:43:59 -0300

    Still fighting with the PUC-Rio class.

--- tex_split.sh
+++ tex_split.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 gawk '/BEGIN LYX '$1'/ { on=1 } { if (on) print } /END LYX '$1'/ { on=0 }' "$2" |\
-sed 's,\\subsection{,\\subsubsection{,g' |\
-sed 's,\\section{,\\subsection{,g;
-     s,\\~,\\textasciitilde{},g;
-     s,—,\\textemdash{},g;'
-
+sed 's,\\subsection{,\\subsubsection{,g' | {
+   if [[ "$1" != "DEMO" ]]
+   then
+      sed 's,\\section{,\\subsection{,g;'
+   else
+      cat
+   fi
+} |\
+   sed 's,\\~,\\textasciitilde{},g;
+        s,—,\\textemdash{},g;'
